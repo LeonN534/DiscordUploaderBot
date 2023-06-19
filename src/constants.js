308f22a8485f0b8bfa256f9fc29c0c572ebd6a7a
LeonN <leo321war@gmail.com>
2023-06-19 18:08:56 -0500

    Reworked bot commands

--- src/constants.js
+++ src/constants.js
@@ -6,7 +6,17 @@ Here are the commands you need to set before using me:`;
 const SET_CHANNEL_COMMAND = `!set channel`;
 const SET_SERVER_IP_COMMAND = `!set server ip`;
 const SET_SERVER_PORT_COMMAND = `set server port`;
-const SET_SERVER_PASSWORD_COMMAND = `!set server password`;
+const ADD_SERVER_USER_COMMAND = `!add server user`;
+const DELETE_SERVER_USER_COMMAND = `!del server password`;
+const LIST_SERVER_USERS_COMMAND = `!list server users`;
 
-export {GREETING_MESSAGE, SET_CHANNEL_COMMAND, SET_SERVER_IP_COMMAND, SET_SERVER_PASSWORD_COMMAND, SET_SERVER_PORT_COMMAND};
+export {
+    GREETING_MESSAGE, 
+    SET_CHANNEL_COMMAND, 
+    SET_SERVER_IP_COMMAND, 
+    SET_SERVER_PORT_COMMAND, 
+    ADD_SERVER_USER_COMMAND, 
+    DELETE_SERVER_USER_COMMAND, 
+    LIST_SERVER_USERS_COMMAND
+};
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import dotenv from 'dotenv';
 import {Client, GatewayIntentBits, Partials, codeBlock} from 'discord.js';
-import {GREETING_MESSAGE, SET_CHANNEL_COMMAND, SET_SERVER_IP_COMMAND, SET_SERVER_PASSWORD_COMMAND, SET_SERVER_PORT_COMMAND} from './constants.js';
+import {
+    GREETING_MESSAGE, 
+    SET_CHANNEL_COMMAND, 
+    SET_SERVER_IP_COMMAND, 
+    SET_SERVER_PORT_COMMAND, 
+    ADD_SERVER_USER_COMMAND, 
+    DELETE_SERVER_USER_COMMAND, 
+    LIST_SERVER_USERS_COMMAND
+} from './constants.js';
 
 dotenv.config();
 
 let botUserAdmin;
-let channelToListen, serverIp, serverPass, serverPort = '21';
+
+const data = {
+    channelToListen,
+    serverIp: "",
+    serverPort: "21",
+    serverUsers: []
+}
 
 const client = new Client({
     intents: [
@@ -36,12 +50,13 @@ client.on("guildCreate", guild => {
 
     guild.fetchAuditLogs({type: 28, limit: 1}).then(log => {
         botUserAdmin.send(`
-        ${codeBlock(SET_CHANNEL_COMMAND)}This command is used to set the channel ID where I'll check for the files to upload. This value is necessary.
-        ${codeBlock(SET_SERVER_IP_COMMAND)}This command is used to set the ip of the remote server I will connect to. This value is necessary.
-        ${codeBlock(SET_SERVER_PORT_COMMAND)}This command is used to set the port of the remote server I will connect to. If you don't set this value, I will try to connect to the port 21.
-        ${codeBlock(SET_SERVER_PASSWORD_COMMAND)}This command is used to set the password of the remote server I will connect to. This value is necessary.
-        In all of the above commands you must ommit the double quotes (" ") and replace argument with the value to set.
-        `).catch(e => console.error(e));
+        ${codeBlock(SET_CHANNEL_COMMAND)}This command is used to set the channel where I'll check for the files to upload. To set the channel, add the channel ID after the command separating it by and space.
+        ${codeBlock(SET_SERVER_IP_COMMAND)}This command is used to set the IP of the remote server I will connect to. To set the IP, add IP direction after the command separating it by and space.
+        ${codeBlock(SET_SERVER_PORT_COMMAND)}This command is used to set the port of the remote server I will connect to. To set the port, add the port number after the command separating it by and space. If you don't set this value, I will try to connect to the port 21.
+        ${codeBlock(ADD_SERVER_USER_COMMAND)}This command is used to add a user of the VPS where the files will be uploaded. After you type this command, the first message will be stored as the username, and the second message as the password for that user. When you type this command, the bot will not work until you send the two messages. You can delete the messages where you wrote you username and password. And don't worry, the passwords are encrypted before being stored.
+        ${codeBlock(LIST_SERVER_USERS_COMMAND)}This command is used to list all the users of the VPS where the bot will upload the files to.
+        ${codeBlock(DELETE_SERVER_USER_COMMAND)}This command is used to delete a user from the list of users where the bot will upload the files to. To delete a user, add username after the command separating it by and space.
+        `);
     });
 });
 
@@ -57,10 +72,10 @@ client.on('messageCreate', async (message) => {
             client.channels.fetch(command[2]).then(
                 (value) => {
                     if(value.type == 0) {
-                        channelToListen = value.id;
+                        data.channelToListen = value.id;
                         botUserAdmin.send(`Channel succesfully set! :smiley:`);
                     } else {
-                        botUserAdmin.send(`That's not a valid channel! :cry:`);
+                        botUserAdmin.send(`That's not a valid text channel! :cry:`);
                     }
                 },
                 (error) => {
@@ -69,14 +84,20 @@ client.on('messageCreate', async (message) => {
             );
         } else if(message.content.startsWith(SET_SERVER_IP_COMMAND)) {
             command = message.content.split(" ");
-        } else if(message.content.startsWith(SET_SERVER_PASSWORD_COMMAND)) {
-            command = message.content.split(" ");
             if(command[2] == "" || command[2] == undefined) return;
-            serverPass = command[2];
+            if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(command[2])) {  
+                botUserAdmin.send(`Server ip succesfully set! :smiley:`);
+            }  
         } else if(message.content.startsWith(SET_SERVER_PORT_COMMAND)) {
             command = message.content.split(" ");
             if(command[2] == "" || command[2] == undefined || !Number.isInteger(Number(command[2]))) return;
-            serverPort = command[2];
+            data.serverPort = command[2];
+            botUserAdmin.send(`Server port succesfully set! :smiley:`);
+        } else if(message.content == LIST_SERVER_USERS_COMMAND) {
+            
+            botUserAdmin.send(``);
+        } else if(message.content == ADD_SERVER_USER_COMMAND) {
+            
         }
     }
 
