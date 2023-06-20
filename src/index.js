9d5585dd498167e5b77eca2b452f88b7b96358f6
LeonN <leo321war@gmail.com>
2023-06-20 16:30:29 -0500

    Programmed delete users command

--- src/index.js
+++ src/index.js
@@ -55,20 +55,21 @@ client.on("guildCreate", async (guild) => {
         ${codeBlock(SET_SERVER_PORT_COMMAND)}This command is used to set the port of the remote server I will connect to. To set the port, add the port number after the command separating it by and space. If you don't set this value, I will try to connect to the port 21.
         ${codeBlock(ADD_SERVER_USER_COMMAND)}This command is used to add a user of the VPS where the files will be uploaded. After you type this command, the first message will be stored as the username, and the second message as the password for that user. When you type this command, the bot will not work until you send the two messages. You can delete the messages where you wrote you username and password. And don't worry, the passwords are encrypted before being stored.
         ${codeBlock(LIST_SERVER_USERS_COMMAND)}This command is used to list all the users of the VPS where the bot will upload the files to.
-        ${codeBlock(DELETE_SERVER_USER_COMMAND)}This command is used to delete a user from the list of users where the bot will upload the files to. To delete a user, add username after the command separating it by and space.
+        ${codeBlock(DELETE_SERVER_USER_COMMAND)}This command is used to delete a user from the list of users where the bot will upload the files to. To delete a user, add the username after the command separating it by and space.
         `);
     });
 });
 
 client.on('messageCreate', async (message) => {
 
+    // Set the value of the testing user
     await client.users.fetch(process.env.TESTING_USER, false).then((user) => {
         botUserAdmin = user;
     })
 
     // Check for DM messages from the BotAdmin 
     if(message.guild == null && message.author.id == process.env.TESTING_USER) {
-           
+           console.log(message.channelId);
         // Check if the bot is waiting for the username or password (Waiting mode)
         if(botWaitMode) {
             //Check if the bot is waiting either for the username or the password
@@ -79,11 +80,15 @@ client.on('messageCreate', async (message) => {
                     break;
                 case 2:
                     auxPassword = message.content;
-                    data.serverUsers.push({
-                        "name": auxUsername,
-                        "password": encrypt(auxPassword)
-                    });
-                    botUserAdmin.send(`:white_check_mark: User succesfully added!`);
+                    if(data.serverUsers.findIndex((element) => {return element.name == auxUsername}) !== -1) {
+                        botUserAdmin.send(`:warning: The user is already added!`);
+                    } else {
+                        data.serverUsers.push({
+                            "name": auxUsername,
+                            "password": encrypt(auxPassword)
+                        });
+                        botUserAdmin.send(`:white_check_mark: User successfully added!`);
+                    }
                     botDataCounter = 1; // Reset the counter to start waiting for the username in the next wait mode
                     botWaitMode = false; // Exit wait mode
                     break;
@@ -100,7 +105,7 @@ client.on('messageCreate', async (message) => {
                     (value) => {
                         if(value.type == 0) {
                             data.channelToListen = value.id;
-                            botUserAdmin.send(`:white_check_mark: Channel succesfully set!`);
+                            botUserAdmin.send(`:white_check_mark: Channel successfully set!`);
                         } else {
                             botUserAdmin.send(`:warning: That's not a valid text channel!`);
                         }
@@ -114,7 +119,7 @@ client.on('messageCreate', async (message) => {
                 if(command[3] == "" || command[3] == undefined) return;
                 if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(command[3])) {  
                     data.serverIp = command[3];
-                    botUserAdmin.send(`:white_check_mark: Server ip succesfully set!`);
+                    botUserAdmin.send(`:white_check_mark: Server ip successfully set!`);
                 } else {
                     botUserAdmin.send(":warning: That's not a valid IP address!"); 
                 }
@@ -125,7 +130,7 @@ client.on('messageCreate', async (message) => {
                     botUserAdmin.send(":warning: That's not a valid port!");   
                 } else {
                     data.serverPort = command[3];
-                    botUserAdmin.send(`:white_check_mark: Server port succesfully set!`);
+                    botUserAdmin.send(`:white_check_mark: Server port successfully set!`);
                 }
             } else if(message.content == LIST_SERVER_USERS_COMMAND) {
                 if(data.serverUsers.length == 0) {
@@ -138,14 +143,32 @@ client.on('messageCreate', async (message) => {
                 }
             } else if(message.content == ADD_SERVER_USER_COMMAND) {
                 botWaitMode = true;
-            }    
+            } else if(message.content.startsWith(DELETE_SERVER_USER_COMMAND)) {
+                command = message.content.split(" ");
+                if(command[3] == "" || command[3] == undefined) return;
+                if(data.serverUsers.length == 0) {
+                    botUserAdmin.send(":warning: There are no user added yet!");   
+                } else {
+                    let coincidence = data.serverUsers.findIndex((element) => {return element.name == command[3]});
+                    if(coincidence !== -1) {
+                        data.serverUsers.splice(coincidence, 1);
+                        botUserAdmin.send(`:white_check_mark: The user has been deleted!`);
+                    } else {
+                        botUserAdmin.send(`:warning: The specified user is does not exist!`);
+                    }
+                }
+            }
+        }        
+    } else if(message.guild !== null && message.channelId == data.channelToListen) {
+        if(data.channelToListen == null || data.serverIp == null || data.serverUsers.length == 0) {
+            message.author.send(`:warning: The bot parameters are not properly configured. I can't upload your files until this issue is fixed. Ask the admin of the server to solve this problem.`);
+        } else {
+            message.type
         }
-        
     }
 
     // Check for messages from the channel to be listened to
-    
-
+    console.log(message.channelId);
 })
 
 function encrypt(text) {
